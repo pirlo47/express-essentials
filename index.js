be7cd401dd467050475a4362d1ee24b10e726afe
pirlo47 <ntoahaep108@gmail.com>
2026-04-17 11:23:00 +0200

    feat: add static image assets and implement express middleware for JSON/URL-encoded parsing and error handling

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const app = express();
 
 const PORT = 3000; 
 
+//Using the public folder at the root (/) of the project
+app.use(express.static('public'));
+
+//Using the images folder are /images
+app.use('/images', express.static('images')); 
+
+//We want our request to be json
+//app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
 //GET
 app.get('/', (req, res) => {
     res.json(data); 
+});
+
+//Post - express.json and express.urlencoded
+app.post('/item', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 //GET with next()
@@ -20,11 +37,13 @@ app.get('/next', (req, res, next) => {
 
 //GET with routing parameters
 app.get("/class/:id", (req, res) => {
+    //Middleware: Access the routing params
     //Use the Number object to turn the student id to a number.
     const studentId = Number(req.params.id);
 
     //using filter, we will filter down to the student id 
     const student = data.filter((student) => student.id === studentId);
+    //Everything above this line is middleware
     res.send(student);
 });
 
@@ -35,7 +54,8 @@ app.get('/redirect', (req, res) => {
 
 //route chaining
 app.route("/class").get((req, res) => {
-    res.send('Retrieve class info');
+    // res.send('Retrieve class info');
+    throw new Error(); 
 }).post((req, res) => {
     res.send('Create Class Info');
 }).put((req, res) => {
@@ -52,6 +72,13 @@ app.route("/class").get((req, res) => {
 // app.delete('/delete', (req, res) => {
 //     res.send('This is a DELETE request at /');
 // });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(`Something is broken!`);
+
+})
+
 app.listen(PORT, () => {
     console.log(`The server is running on port: ${PORT}`);
     //console.log(data);
